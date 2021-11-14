7189176527453b9db3f3c48f535cb9d41979ccd5
Kolmagin Danil <dkolmagin@gmail.com>
2021-11-14 14:51:38 +0200

    Add favicon + first random Histogram

--- lab1/main.js
+++ lab1/main.js
@@ -1,50 +1,53 @@
-import './theme.js';
+// import './theme.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  Highcharts.chart({
-    chart: {
-      renderTo: 'container',
-      type: 'bar',
-      backgroundColor: {
-        linearGradient: [0, 0, 500, 500],
-        stops: [
-          [0, 'rgb(255, 255, 255)'],
-          [1, 'rgb(240, 240, 255)'],
-        ],
-      },
-      borderWidth: 2,
-      plotBackgroundColor: 'rgba(255, 255, 255, .9)',
-      plotShadow: true,
-      plotBorderWidth: 1,
+// https://www.highcharts.com/docs/chart-and-series-types/histogram-series
+
+const data = new Array(10000).fill(0).map(Math.random);
+
+Highcharts.chart({
+  chart: {
+    renderTo: 'container',
+  },
+
+  title: {
+    text: 'Lab1',
+  },
+
+  xAxis: [{
+    title: { text: 'Data' },
+    alignTicks: false,
+  }, {
+    title: { text: 'Histogram' },
+    alignTicks: false,
+    opposite: true,
+  }],
+
+  yAxis: [{
+    title: { text: 'Data' },
+  }, {
+    title: { text: 'Histogram' },
+    opposite: true,
+  }],
+
+  series: [{
+    name: 'Histogram',
+    type: 'histogram',
+    xAxis: 1,
+    yAxis: 1,
+    baseSeries: 's1',
+    zIndex: -1,
+  }, {
+    name: 'Data',
+    type: 'scatter',
+    data: data,
+    id: 's1',
+    marker: {
+      radius: 1.5,
     },
-    title: {
-      text: 'Fruit Consumption',
-    },
-    yAxis: {
-      title: {
-        text: 'Fruit eaten',
-      },
-    },
-    // xAxis: {
-    //   categories: ['Apples', 'Bananas', 'Oranges'],
-    // },
-    // series: [{
-    //   name: 'Jane',
-    //   data: [1, 0, 4],
-    // }, {
-    //   name: 'John',
-    //   data: [5, 7, 3],
-    // }],
-    xAxis: {
-      type: 'datetime',
-    },
-    series: [{
-      data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
-      pointStart: Date.UTC(2010, 0, 1),
-      pointInterval: 3600 * 1000, // one hour
-    }],
-    credits: {
-      enabled: false
-    },
-  });
+    turboThreshold: 10000,
+  }],
+
+  credits: {
+    enabled: false,
+  },
 });
