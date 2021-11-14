9651e49e77148194ad1fdf88e3ba59ae7b0ee63f
Kolmagin Danil <dkolmagin@gmail.com>
2021-11-14 13:29:55 +0200

    Some more examples

--- lab1/main.js
+++ lab1/main.js
@@ -1,39 +1,50 @@
-let chart; // globally available
+import './theme.js';
 
-document.addEventListener('DOMContentLoaded', function() {
-  chart = Highcharts.stockChart('container', {
-    rangeSelector: {
-      selected: 1,
-    },
-    series: [{
-      name: 'USD to EUR',
-      data: usdtoeur, // predefined JavaScript array
-    }],
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  const chart = Highcharts.chart('container', {
+document.addEventListener('DOMContentLoaded', () => {
+  Highcharts.chart({
     chart: {
+      renderTo: 'container',
       type: 'bar',
+      backgroundColor: {
+        linearGradient: [0, 0, 500, 500],
+        stops: [
+          [0, 'rgb(255, 255, 255)'],
+          [1, 'rgb(240, 240, 255)'],
+        ],
+      },
+      borderWidth: 2,
+      plotBackgroundColor: 'rgba(255, 255, 255, .9)',
+      plotShadow: true,
+      plotBorderWidth: 1,
     },
     title: {
       text: 'Fruit Consumption',
-    },
-    xAxis: {
-      categories: ['Apples', 'Bananas', 'Oranges'],
     },
     yAxis: {
       title: {
         text: 'Fruit eaten',
       },
     },
+    // xAxis: {
+    //   categories: ['Apples', 'Bananas', 'Oranges'],
+    // },
+    // series: [{
+    //   name: 'Jane',
+    //   data: [1, 0, 4],
+    // }, {
+    //   name: 'John',
+    //   data: [5, 7, 3],
+    // }],
+    xAxis: {
+      type: 'datetime',
+    },
     series: [{
-      name: 'Jane',
-      data: [1, 0, 4],
-    }, {
-      name: 'John',
-      data: [5, 7, 3],
+      data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+      pointStart: Date.UTC(2010, 0, 1),
+      pointInterval: 3600 * 1000, // one hour
     }],
+    credits: {
+      enabled: false
+    },
   });
 });
