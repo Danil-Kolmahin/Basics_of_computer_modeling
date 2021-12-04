aca143017a4467fa2b251a49974b291f62638a64
Kolmagin Danil <dkolmagin@gmail.com>
2021-12-04 19:16:06 +0200

    Lab2

--- lab2/main.js
+++ lab2/main.js
@@ -5,6 +5,8 @@ class Parent {
     this.content = new Array(isFinite(capacity) ? capacity : 9000).fill(null);
   }
 
+  get canAdd() {}
+
   addOne() {}
 
   takeOne() {}
@@ -14,12 +16,44 @@ class Que extends Parent {
   constructor(capacity = Infinity, isBlocked = false) {
     super(capacity, isBlocked);
   }
+
+  get canTake() {
+    return true;
+  }
+
+  get canAdd() {
+    return this.capacity !== this.content.length;
+  }
+
+  takeOne() {
+    const res = this.content[this.content.length - 1];
+    this.content[this.content.length - 1] = null;
+    return res;
+  }
+
+  addOne(instance) {
+    this.content[this.content.length] = instance;
+  }
 }
 
 class Channel extends Parent {
   constructor(mu, isBlocked = false) {
     super(1, isBlocked);
     this.mu = mu;
+  }
+
+  get canTake() {
+    return this.content[0] === null;
+  }
+
+  get canAdd() {
+    return this.content[0].rest <= 0;
+  }
+
+  takeOne() {
+    const res = this.content[0];
+    this.content[0] = null;
+    return res;
   }
 
   addOne(instance) {
@@ -30,6 +64,11 @@ class Channel extends Parent {
   spend(step) {
     if (this.content[0]) this.content[0].rest -= step;
   }
+
+  resolve() {
+    if (this.content[0]) this.content[0].resolve();
+    this.content[0] = null;
+  }
 }
 
 class Instance {
@@ -38,14 +77,34 @@ class Instance {
   constructor(index) {
     this.index = index;
   }
+
+  resolve() {}
 }
 
 const process = (lambda, way, maxTime, step = 0.05) => {
+  let count = 0;
   for (let time = 0; time < maxTime; time += step) {
     way.forEach((element) => {
       if (Array.isArray(element))
         element.forEach((channel) => channel.spend(step));
     });
+
+    count += step;
+    let newElement;
+    if (count >= lambda) newElement = new Instance(time);
+
+    for (let i = way.length - 1; i >= 0; i--) {
+      const cur = way[i];
+      if (Array.isArray(cur)) {
+        cur.forEach((elem) => {
+          if (elem.canTake) {
+            if (i === way.length - 1) elem.resolve();
+            else if (way[i + 1].canAdd) way[i + 1].addOne(elem.takeOne());
+          }
+        });
+      } else {
+      }
+    }
   }
 };
 
