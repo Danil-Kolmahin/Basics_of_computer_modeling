b51d8f51f99146793445da2d7c0fef44a76dcc76
Kolmagin Danil <dkolmagin@gmail.com>
2021-12-04 18:42:19 +0200

    Lab2

--- lab2/main.js
+++ lab2/main.js
@@ -1,111 +1,61 @@
-'use strict';
+class Parent {
+  constructor(capacity = 1, isBlocked = false) {
+    this.capacity = capacity;
+    this.isBlocked = isBlocked;
+    this.content = new Array(isFinite(capacity) ? capacity : 9000).fill(null);
+  }
+
+  addOne() {}
+
+  takeOne() {}
+}
+
+class Que extends Parent {
+  constructor(capacity = Infinity, isBlocked = false) {
+    super(capacity, isBlocked);
+  }
+}
+
+class Channel extends Parent {
+  constructor(mu, isBlocked = false) {
+    super(1, isBlocked);
+    this.mu = mu;
+  }
+
+  addOne(instance) {
+    this.content[0] = instance;
+    instance.rest = instance.executionTime;
+  }
+
+  spend(step) {
+    if (this.content[0]) this.content[0].rest -= step;
+  }
+}
 
 class Instance {
   executionTime = 1;
-  index;
-  isProceed = false;
 
-  constructor({ index } = {}) {
+  constructor(index) {
     this.index = index;
   }
-
-  resolve() {
-    console.log({ index: this.index });
-    this.isProceed = true;
-  }
 }
 
-class Que {
-  length = Infinity;
-  content = [];
-
-  constructor({ length } = {}) {
-    if (typeof length === 'number') this.length = length;
+const process = (lambda, way, maxTime, step = 0.05) => {
+  for (let time = 0; time < maxTime; time += step) {
+    way.forEach((element) => {
+      if (Array.isArray(element))
+        element.forEach((channel) => channel.spend(step));
+    });
   }
+};
 
-  get isFull() {
-    return this.content.length === this.length;
-  }
-
-  addOne(instance) {
-    if (this.isFull) throw new Error('isFull');
-    this.content.push(instance);
-  }
-
-  takeOne() {
-    return this.content.pop() || null;
-  }
-}
-
-class Channel {
-  intensity;
-  onProceed = null;
-
-  constructor({ intensity } = {}) {
-    this.intensity = intensity;
-  }
-
-  get isEmpty() {
-    return this.onProceed === null;
-  }
-
-  addOne(instance) {
-    if (this.onProceed !== null) throw new Error('Crowded');
-    this.onProceed = instance;
-    return this;
-  }
-
-  proceed() {
-    if (!this.isEmpty) this.onProceed.resolve();
-    return this;
-  }
-
-  takeOne() {
-    if (!this.isEmpty) {
-      const onProceed = this.onProceed;
-      this.onProceed = null;
-      return onProceed;
-    }
-  }
-}
-
-const timeout = (time) => new Promise((resolve) => setTimeout(resolve, time));
-
-(async () => {
-  const WORK_TIME = 5 * 1000;
-  const ONE_STEP_DELAY = 1000;
-  const START_TIME = Date.now();
-
-  let count = 0;
-  const LAMBDA = 5;
-  const MU1 = 5;
-  const MU2 = 3;
-  const L = 8;
-
-  const QUE1 = new Que();
-  const QUE2 = new Que({ length: L });
-  const K1 = [new Channel({ intensity: MU1 }), new Channel({ intensity: MU1 })];
-  const K2 = [new Channel({ intensity: MU2 }), new Channel({ intensity: MU2 })];
-
-  while (true) {
-    const newInstances = new Array(LAMBDA)
-      .fill()
-      .map(() => new Instance({ index: count++ }));
-
-    for (let i = 0; i < LAMBDA; i++) {
-      QUE1.addOne(newInstances[i]);
-      if (K1[0].isEmpty) K1[0].addOne(QUE1.takeOne());
-      if (K1[1].isEmpty) K1[1].addOne(QUE1.takeOne());
-      K1[0].proceed();
-      K1[1].proceed();
-
-      if (!QUE2.isFull) QUE2.addOne(K1[0].takeOne());
-      if (!QUE2.isFull) QUE2.addOne(K1[1].takeOne());
-      if (K2[0].isEmpty) K2[0].addOne(QUE2.takeOne()).proceed().takeOne();
-      if (K2[1].isEmpty) K2[1].addOne(QUE2.takeOne()).proceed().takeOne();
-    }
-
-    await timeout(ONE_STEP_DELAY);
-    if (START_TIME + WORK_TIME < Date.now()) return;
-  }
-})();
+process(
+  5,
+  [
+    new Que(),
+    [new Channel(5), new Channel(5)],
+    new Que(8, true),
+    [new Channel(3), new Channel(3)],
+  ],
+  5
+);
